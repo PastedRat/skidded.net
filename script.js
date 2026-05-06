39cb1c083eb483740342fa3b18e0d1554fe7bb31
PastedRat <145750443+PastedRat@users.noreply.github.com>
2026-05-06 23:22:40 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const toggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+if (toggle && menu) {
+  toggle.addEventListener('click', () => {
+    const expanded = toggle.getAttribute('aria-expanded') === 'true';
+    toggle.setAttribute('aria-expanded', String(!expanded));
+    menu.classList.toggle('open');
+  });
+}
